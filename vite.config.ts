22658fc16d1b906c6d13f98cf1231797a15822d7
matb85 <mateuszbis85@gmail.com>
2024-06-13 18:16:19 +0200

    fix: :green_heart: cd config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(env => ({
   plugins: [react()],
-});
+  base: env.mode == "production" ? "/AVsystem-home-assignment/" : "/",
+}));
