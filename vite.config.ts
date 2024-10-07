064cf08ddfebd6df2eda5acca1d9f3423b21e64d
matb85 <mateuszbis85@gmail.com>
2024-10-07 17:48:35 +0200

    fix: :bug: base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(env => ({
   plugins: [react()],
-  base: env.mode == "production" ? "/AVsystem-home-assignment/" : "/",
+  base: env.mode == "production" ? "/elevator-simulator/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/algorithm"),
